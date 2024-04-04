e38765857ba72d044c668f013c09095b9c0c12e7
Vito <vitobernal@hotmail.com>
2024-04-04 17:18:16 -0300

    Agregado archivo Main.java y clase Main con variables y cálculo de promedio

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+        // Declaración e inicialización de variables
+        int num1 = 8;
+        int num2 = 5;
+        int num3 = 1;
+        int num4 = 20;
+        int num5 = 9;
+        
+        // Cálculo del promedio
+        double promedio = (num1 + num2 + num3 + num4 + num5) / 5.0;
+        
+        // Mostrar el resultado por consola
+        System.out.println("El promedio es: " + promedio);
+		
+		
+	}
+
+}
